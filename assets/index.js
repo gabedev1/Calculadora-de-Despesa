c740633373f2f0f3a1cb0904f9fe21554905be57
gabedev1 <gabrielssilvac08@gmail.com>
2025-10-03 22:30:52 -0300

    update of the new functions for the application

--- assets/index.js
+++ assets/index.js
@@ -4,6 +4,25 @@ const saldoValue = document.getElementById("saldo-value");
 const totalDespesasValue = document.getElementById("total-despesas-value");
 const totalDespesasBox = document.querySelector(".total-despesas-box");
 
+const addBotao = document.getElementById("adicionar");
+const container = document.getElementById("div-inputs");
+
+//função para adicionar as novas despesas / adicionar elas na div e adicionar no array despesas
+addBotao.addEventListener("click", () => {
+  const placeholder = prompt("Digite sua nova despesa");
+
+  if (placeholder) {
+    const novaDespesa = document.createElement("input");
+    novaDespesa.type = "number";
+    novaDespesa.placeholder = placeholder;
+    container.appendChild(novaDespesa);
+
+    // marca o input como dinâmico e guarda o nome
+    novaDespesa.classList.add("input-group", "essenciais", "dynamic-input");
+    novaDespesa.dataset.nome = placeholder;
+  }
+});
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
@@ -15,6 +34,19 @@ form.addEventListener("submit", function (e) {
   const educacao = Number(document.getElementById("educacao").value);
   const investimento = Number(document.getElementById("investimento").value);
 
+  // essa const coleta os inputs dinamicos 
+  const dinamicas = Array.from(
+    container.querySelectorAll("input.dynamic-input") // procura todos os elementos com a seguinte class e retorna uma NodeList
+  ).map((i) => ({
+    nome: i.dataset.nome || i.placeholder,
+    valor: Number(i.value) || 0,
+  }));
+
+  //array.from muda a NodeList para um array para poder usar o map
+  //o map cria um objeto para cada input com duas regras : nome e valor
+  //o nome tenta usar o nome da despesa se não houver, ele usa o PlaceHolder
+  //o valor converte o valor para numero se der errado retorna NaN ou 0
+
   const despesas = [
     { nome: "Moradia", valor: moradia },
     { nome: "Transporte", valor: transporte },
@@ -22,10 +54,10 @@ form.addEventListener("submit", function (e) {
     { nome: "Alimentação", valor: alimentacao },
     { nome: "Educação", valor: educacao },
     { nome: "Investimento", valor: investimento },
+    ...dinamicas,
   ];
 
   const totalDespesas = despesas.reduce((acc, d) => acc + d.valor, 0);
-
 
   //parte que cria a lista com as despesas no HTML, mapeando as despesas por ordem alfabetica
   listaDespesas.innerHTML = despesas
@@ -41,7 +73,6 @@ form.addEventListener("submit", function (e) {
 
   //função pra deixar o total de despesas visivel quando clicar no botão de adicionar
   totalDespesasBox.classList.add("visible");
-
 
   //lógica resposável por identificar se o saldo é positivo ou negativo e mudar a cor dos elementos de acordo com o resultado
   const saldo = rendaLiquida - totalDespesas;
